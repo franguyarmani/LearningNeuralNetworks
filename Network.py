85a01092749e13474d79ba43efa3e2940a271efa
franguyarmani <fbpeabody@gmail.com>
2018-09-17 19:55:15 -0400

    First commit

---
+++ Network.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class Network(object):
+
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        sefl.biases = [np.random.randn(
