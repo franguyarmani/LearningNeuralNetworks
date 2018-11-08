d0b4682b716b2bb268eeb859ebde1aac9e52a835
franguyarmani <fbpeabody@gmail.com>
2018-11-08 15:00:15 -0500

    update scraper to return aggregated data frame for all sp500 companies

--- S&P500/Scraper.py
+++ S&P500/Scraper.py
@@ -41,7 +41,7 @@ def get_data_from_yahoo(reload_tickers=False, reload_data = False):
     if not os.path.exists('sp500stocks'):
         os.makedirs('sp500stocks')
     if reload_data:
-        for ticker in tickers:
+        for ticker in tqdm(tickers):
             df = web.DataReader(ticker,'yahoo',start,end)
             df.to_csv('sp500stocks/{}.csv'.format(ticker))
     else:
@@ -57,11 +57,46 @@ def get_data_from_yahoo(reload_tickers=False, reload_data = False):
                     print("ERROR:{} code not be downloaded".format(ticker)) 
 
 
+def aggregate_data():
+    with open("sp500tickers.pickle","rb") as f:
+        tickers = pickle.load(f)
+    main_df = pd.DataFrame()
+
+    for count, ticker in tqdm(enumerate(tickers)):
+        df = pd.DataFrame()
+        try:
+            df = pd.read_csv("sp500stocks/{}.csv".format(ticker))
+        except FileNotFoundError :
+            print("Data for {} was not found".format(ticker))
+            continue
+        df.set_index("Date", inplace=True)
+
+        df.rename(columns = {'Adj Close': ticker}, inplace=True)
+        df.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer') #keeps rows that do not exist in other data frame
+
+#        if count % 10 == 0:
+#            print(count)
+
+    print("++++++++++++++++++HEAD++++++++++++++++++")
+    print(main_df.head())
+    print("++++++++++++++++++TAIL++++++++++++++++++")
+    print(main_df.tail())
+    main_df.to_csv("sp500_aggregated_closes.csv")
+
+
+
+
+
 start = dt.datetime(1985,1,1)
 end = dt.datetime(2016,12,31)
 
-testdf = web.DataReader('LIN','yahoo',start,end)
-print(testdf.head())
-get_data_from_yahoo()
+
+#get_data_from_yahoo()
+aggregate_data()
 
 
